de4f27572461a162c303235595e98164b519007b
Euhow <euhowzy@gmail.com>
2012-10-25 17:18:04 +0800

    New feature:     advanced push requesting way
    Signed-off-by: Euhow <euhowzy@gmail.com>

--- mobiquo/push_hook.php
+++ mobiquo/push_hook.php
@@ -190,39 +190,37 @@ function tapatalk_push_pm()
 function tt_do_post_request($data)
 {
     $push_url = 'http://push.tapatalk.com/push.php';
+    $push_host = 'push.tapatalk.com';
     $response = 'CURL is disabled and PHP option "allow_url_fopen" is OFF. You can enable CURL or turn on "allow_url_fopen" in php.ini to fix this problem.';
-    if (function_exists('curl_init'))
+
+    if (ini_get('allow_url_fopen'))
+    {
+        $fp = fsockopen($push_host, 80, &$errno, &$errstr, 5);
+        
+        if(!$fp)
+            return false;
+            
+        $data =  http_build_query($data);
+        
+        fputs($fp, "POST /push.php HTTP/1.1\r\n");
+        fputs($fp, "Host: $push_host\r\n");
+        fputs($fp, "Content-type: application/x-www-form-urlencoded\r\n");
+        fputs($fp, "Content-length: ". strlen($data) ."\r\n");
+        fputs($fp, "Connection: close\r\n\r\n");
+        fputs($fp, $data);
+        fclose($fp);
+    }
+    elseif (function_exists('curl_init'))
     {
         $ch = curl_init($push_url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_TIMEOUT,10);
-
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT,1);
         $response = curl_exec($ch);
         curl_close($ch);
-    }
-    elseif (ini_get('allow_url_fopen'))
-    {
-        $params = array('http' => array(
-            'method' => 'POST',
-            'content' => http_build_query($data, '', '&'),
-        ));
-
-        $ctx = stream_context_create($params);
-        $timeout = 10;
-        $old = ini_set('default_socket_timeout', $timeout);
-        $fp = @fopen($push_url, 'rb', false, $ctx);
-
-        if (!$fp) return false;
-
-        ini_set('default_socket_timeout', $old);
-        stream_set_timeout($fp, $timeout);
-        stream_set_blocking($fp, 0); 
-        
-
-        $response = @stream_get_contents($fp);
     }
     return $response;
 }
