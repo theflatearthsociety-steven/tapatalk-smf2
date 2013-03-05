90e2257b73d9712d36031af024cb5c8c68947573
Euhow <euhowzy@gmail.com>
2013-03-05 17:45:32 +0800

    +/- update tapatalk setting icon; update tapadetect.js; remove crome js; Add options for kindle fire HD.
    Signed-off-by: Euhow <euhowzy@gmail.com>

--- Sources/ManageTapatalk.php
+++ Sources/ManageTapatalk.php
@@ -245,6 +245,8 @@ function ManageTapatalkRebranding($return_config = false)
             array('text', 'tp_android_url', 'value'=> isset($modSettings['tp_android_url'])? $modSettings['tp_android_url'] : 'market://details?id=com.quoord.tapatalkpro.activity' ,  'size' => '80'),
             array('text', 'tp_kf_msg', 'value'=> isset($modSettings['tp_kf_msg'])? $modSettings['tp_kf_msg'] : 'This forum has an app for Kindle Fire! Click OK to learn more about Tapatalk.' ,  'size' => '80'),
             array('text', 'tp_kf_url', 'value'=> isset($modSettings['tp_kf_url'])? $modSettings['tp_kf_url'] : 'http://www.amazon.com/gp/mas/dl/android?p=com.quoord.tapatalkpro.activity' ,  'size' => '80'),
+            array('text', 'tp_kf_hd_msg', 'value'=> isset($modSettings['tp_kf_hd_msg'])? $modSettings['tp_kf_hd_msg'] : 'This forum has an app for Kindle Fire! Click OK to learn more about Tapatalk.' ,  'size' => '80'),
+            array('text', 'tp_kf_hd_url', 'value'=> isset($modSettings['tp_kf_hd_url'])? $modSettings['tp_kf_hd_url'] : 'http://www.amazon.com/gp/mas/dl/android?p=com.quoord.tapatalkHD' ,  'size' => '80'),
     );
 
     if ($return_config)
