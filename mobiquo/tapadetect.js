c3a94f053bf6206fb0bea88a2edc7e394fa9473c
Euhow <euhowzy@gmail.com>
2013-03-08 14:55:48 +0800

    +/- make some fixes
    Signed-off-by: Euhow <euhowzy@gmail.com>

--- mobiquo/tapadetect.js
+++ mobiquo/tapadetect.js
@@ -10,18 +10,20 @@ function detectTapatalk() {
                 if ((tapatalk_ipad_msg != '') && confirm(tapatalk_ipad_msg))
                     window.location = tapatalk_ipad_url;
             } else if(navigator.userAgent.match(/Silk/)) {
-                setTapatalkCookies();
-                if ((tapatalk_kindle_msg != '') && confirm(tapatalk_kindle_msg) && navigator.userAgent.match(/Android 2/i))
-                    window.location = tapatalk_kindle_url;
-                else if((tapatalk_kindle_hd_msg != '') && confirm(tapatalk_kindle_hd_msg) && navigator.userAgent.match(/Android 4/i))
-                	window.location = tapatalk_kindle_hd_url;
+                if (navigator.userAgent.match(/Android 2/i)) {
+                    setTapatalkCookies();
+                    if ((tapatalk_kindle_msg != '') && confirm(tapatalk_kindle_msg))
+                        window.location = tapatalk_kindle_url;
+                }
+                else if(navigator.userAgent.match(/Android 4/i)) {
+                    setTapatalkCookies();
+                    if ((tapatalk_kindle_hd_msg != '') && confirm(tapatalk_kindle_hd_msg))
+                        window.location = tapatalk_kindle_hd_url;
+                }
             } else if(navigator.userAgent.match(/Android/i)) {
-            	if(navigator.userAgent.match(/mobile/i))
-            	{
 	                setTapatalkCookies();
 	                if ((tapatalk_android_msg != '') && confirm(tapatalk_android_msg))
 	                    window.location = tapatalk_android_url;
-            	}
             } else if(navigator.userAgent.match(/BlackBerry/i)) {
                 setTapatalkCookies();
                 if (confirm("This forum has an app for BlackBerry! Click OK to learn more about Tapatalk."))
