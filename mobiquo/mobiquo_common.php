a140aa3c66b2f1a4b881b0091fd4a92bb7a11484
Jayeley <jayeley@gmail.com>
2014-06-30 11:02:34 +0800

    fix error report issue

--- mobiquo/mobiquo_common.php
+++ mobiquo/mobiquo_common.php
@@ -203,9 +203,9 @@ function get_error($err_str)
 
     if(!headers_sent())
     {
-        header('200 OK');
-        header('Mobiquo_is_login:'.($context['user']['is_logged'] ? 'true' : 'false'));
-        header('Content-Type: text/xml');
+        @header('HTTP/1.1 200 OK');
+        @header('Mobiquo_is_login:'.($context['user']['is_logged'] ? 'true' : 'false'));
+        @header('Content-Type: text/xml');
     }
 
     $response = new xmlrpcresp(
